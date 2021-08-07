371e02d20db786e9b2943da1d43cd115ce0359d9
Ubu Untu <woerner99@gmail.com>
2021-08-06 21:03:18 -0500

    changed formatting of printing

--- call_profit.py
+++ call_profit.py
@@ -44,29 +44,32 @@ def boot():
 
 
 def menu():
-	print("\nMAIN MENU\n---------\n(1) Calculate profit, loss, and break-even point of a call\n")
+	print("\nMAIN MENU\n---------\n(1) Calculate profit, loss, and break-even point of a call\n---------------------------------\n(0) Exit Program\n")
 	
 # Get data of User's call and save for later use
 def call_info():
+	clear()
 	print("Please enter the following: <strike price> <amount of calls purchased> <price per call>")
 	data = input()
 	
 	temp = data.split()
 	strike = float(temp[0])
-	call_amount = int(temp[1])
+	call_amount = float(temp[1])
 	call_price = float(temp[2])
 	breakeven = float(strike) + float(call_price)
+	breakeven_format = "{:.2f}".format(breakeven) 
 	
 	#put data of the call into a list that can be appended to the portfolio list
 	call_data = [strike, call_amount, call_price, breakeven]
-	premium = 100 * (call_price * call_amount)
+	premium = float(100 * (call_price * call_amount))
+	premium_format = "{:.2f}".format(premium)  
 	
 
 	print("\n**************************************************************")
 	print("CALL DATA")
 	print("------------------")
-	print("Break-Even price: $"+str(breakeven))
-	print("Premium paid: $"+str(premium))
+	print("Break-Even price: $"+str(breakeven_format))
+	print("Premium paid: $"+str(premium_format))
 	
 	print("|*********************************|")
 	print("|      POTENTIAL PROFIT           |")
@@ -74,7 +77,7 @@ def call_info():
 	print("| Final Price   |    Profit       |")
 	print("|*********************************|")
 
-	for x in range(40):
+	for x in range(35):
 	
 		# Increment each iteration by 10% (the first iteration by just 1 % to get past breakeven)
 		if x == 0:
@@ -83,10 +86,12 @@ def call_info():
 			final_price *= 1.1
 		# Calculate profit and then format price for printing 	
 		profit = (final_price - strike - call_price)*(call_amount*100)
+		profit_format = "{:.2f}".format(profit)  
 		final_price_format = "{:.2f}".format(final_price)  
-		print("| $"+str(final_price_format)+"             $"+str(round(profit,2)))
+		print("| $"+str(final_price_format)+"        |    $"+str(profit_format))
 	
 	print(input("\nPress ENTER to return..."))
+	clear()
 	
 	
 ####################################################################################	
@@ -99,6 +104,8 @@ while True:
 	
 	if choice == '1':
 		call_info()
+	if choice == '0':
+		exit()
 
 	#clear()
 	
